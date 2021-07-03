249a57721d0c5a4de71a725c8f21784d3216a4d3
Sheena528 <71921513+Sheena528@users.noreply.github.com>
2021-07-03 18:35:08 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 showMenu('nav-toggle','nav-menu')
 /*===== MENU SHOW =====*/ 
 const showMenu = (toggleId, navId) =>{
@@ -80,6 +79,7 @@ sr.reveal('.contact__subtitle', {})
 sr.reveal('.contact__text', {interval: 200})
 sr.reveal('.contact__input', {delay: 400})
 sr.reveal('.contact__button', {delay: 600})
+
 
 
 
